26146cc7993fac0d91f8688fbfd39866e18d53de
Frank Batur <frank_batur@outlook.com>
2023-03-26 16:49:33 -0400

    implement validation

--- src/main/java/com/frank/employeeregistrationproject/model/Employee.java
+++ src/main/java/com/frank/employeeregistrationproject/model/Employee.java
@@ -3,6 +3,10 @@ package com.frank.employeeregistrationproject.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -11,7 +15,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 public class Employee {
+
+ //   @NotNull  //accept everything except null  Ex: ""
+ //   @NotEmpty //accept everything except null and empty  Ex: " "
+    @NotBlank //accept everything except null and empty, blank Ex: " 1"
+    @Size(max = 12, min=2)
     private String firstName;
+    @Size(max = 15, min=2)
     private String lastName;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
