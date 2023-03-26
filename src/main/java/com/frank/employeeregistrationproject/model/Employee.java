a08b3a82b5732f8ccd3820685c27eb94a774a2e3
Frank Batur <frank_batur@outlook.com>
2023-03-25 21:18:49 -0400

    Initial commit

---
+++ src/main/java/com/frank/employeeregistrationproject/model/Employee.java
@@ -0,0 +1,23 @@
+package com.frank.employeeregistrationproject.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+    private String firstName;
+    private String lstName;
+    private LocalDate birthDate;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String stage;
+    private String zipCode;
+}
---
+++ src/test/java/com/frank/employeeregistrationproject/EmployeeRegistrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.employeeregistrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
